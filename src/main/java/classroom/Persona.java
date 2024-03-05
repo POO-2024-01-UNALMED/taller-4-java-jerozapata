2de126c2adac4fc52c823ea990f3e49b2d6e23e6
jerozapata <159493286+jerozapata@users.noreply.github.com>
2024-03-04 21:54:22 -0500

    toma 5

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -8,7 +8,7 @@ public class Persona {
     
     static {
         totalPersonas = 0;
-        cedula = 3;
+        
     }
 
     public Persona(long cedula, String nombre) {
@@ -31,6 +31,7 @@ public class Persona {
 
     public Persona(String nombre) {
         this.nombre = "";
+        this.cedula = 0;
         totalPersonas++;
     }
     
